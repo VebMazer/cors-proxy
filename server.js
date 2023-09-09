c2d44f8f17bd5743303e093c13cc8efe7d34feb7
VebMazer <veikko.laine@gmail.com>
2023-09-09 15:14:07 +0300

    Added error handling for the whole request, and some comments.

--- server.js
+++ server.js
@@ -20,36 +20,40 @@ const app = express()
 app.use(cors())
 app.use(requestLogger)
 
-
+// Handle all incoming requests.
 app.all('*', (req, res) => {
+    try {
 
-    if (req.method === 'OPTIONS') {
+        if (req.method === 'OPTIONS') {
 
-        // Preflight request handling
-        res.send()
+            // Preflight request handling.
+            res.send()
     
-    } else {
+        } else {
         
-        // Remove the '/' character from the start of req.url
-        // After that it only contains the target sites url.
-        const url = req.url.slice(1)
+            // Remove the '/' character from the start of req.url
+            // After that it only contains the target sites url.
+            const url = req.url.slice(1)
 
-        // User-Agent header is required by some websites.
-        let headers = {
-            'User-Agent': 'cors-proxy/1.0'
-        }
-
-        // Send a request to the target url and pipe the response
-        // back to the client.
-        request(
-            { url, method: req.method, headers },
-            (error, response, body) => {
-                if (error) console.error(error.message)
+            // User-Agent header is required by some websites.
+            let headers = {
+                'User-Agent': 'cors-proxy/1.0'
             }
-        ).pipe(res)
-    }
+
+            // Send a request to the target url and pipe the response
+            // back to the client.
+            request(
+                { url, method: req.method, headers },
+                (error, response, body) => {
+                    if (error) console.error(error.message)
+                }
+            ).pipe(res)
+        }
+    
+    } catch (error) { console.error(error.message) }
 })
 
+// Start the server.
 app.listen(PORT, () => {
-    console.log(`cors-proxy server running on port ${PORT}`)
+    console.log(`cors-proxy server running on port ${PORT || 3100}`)
 })
